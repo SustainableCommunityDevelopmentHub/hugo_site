73cbb268494d059d2d0a94220022d91d9080f236
G. Willson <greg@biomassiv.es>
2021-01-14 16:43:02 -0700

    Update script.js

--- static/peacewater_form/script.js
+++ static/peacewater_form/script.js
@@ -1,17 +1,24 @@
-$("input[name='drinkingwater']").on("click", function() {
-    $("#DrinkingwaterServiceOptions").toggleClass('show');
-    $("#WastewaterServiceOptions").toggleClass('show');
-    $("#SolidwasteServiceOptions").toggleClass('show');
-});
+$(document).ready(function() {
 
-$("input[name='wastewater']").on("click", function() {
-    $("#DrinkingwaterServiceOptions").toggleClass('hide');
-    $("#WastewaterServiceOptions").toggleClass('show');
-    $("#SolidwasteServiceOptions").toggleClass('hide');
-});
+ $(".DrinkingwaterServiceOptions").hide();
+ $(".WastewaterServiceOptions").hide();
+ $(".SolidwasteServiceOptions").hide();
 
-$("input[name='solidwaste']").on("click", function() {
-    $("#DrinkingwaterServiceOptions").toggleClass('hide');
-    $("#WastewaterServiceOptions").toggleClass('hide');
-    $("#SolidwasteServiceOptions").toggleClass('show');
+ $("input[name='drinkingwater']").on("click", function() {
+    $(".DrinkingwaterServiceOptions").toggleClass('show');
+    $(".WastewaterServiceOptions").toggleClass('show');
+    $(".SolidwasteServiceOptions").toggleClass('show');
+ });
+
+ $("input[name='wastewater']").on("click", function() {
+    $(".DrinkingwaterServiceOptions").toggleClass('hide');
+    $(".WastewaterServiceOptions").toggleClass('show');
+    $(".SolidwasteServiceOptions").toggleClass('hide');
+ });
+
+ $("input[name='solidwaste']").on("click", function() {
+    $(".DrinkingwaterServiceOptions").toggleClass('hide');
+    $(".WastewaterServiceOptions").toggleClass('hide');
+    $(".SolidwasteServiceOptions").toggleClass('show');
+ });
 });
