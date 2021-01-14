0b1624e8969f4889e362887a79bc7bd14a45cfad
G. Willson <greg@biomassiv.es>
2021-01-14 16:38:52 -0700

    Update script.js

--- static/peacewater_form/script.js
+++ static/peacewater_form/script.js
@@ -1,3 +1,17 @@
-$("input[name='product_CD']").on("click", function() {
-  $("#hiddenRow").toggleClass('show');
+$("input[name='drinkingwater']").on("click", function() {
+    $("#DrinkingwaterServiceOptions").toggleClass('show');
+    $("#WastewaterServiceOptions").toggleClass('show');
+    $("#SolidwasteServiceOptions").toggleClass('show');
+});
+
+$("input[name='wastewater']").on("click", function() {
+    $("#DrinkingwaterServiceOptions").toggleClass('hide');
+    $("#WastewaterServiceOptions").toggleClass('show');
+    $("#SolidwasteServiceOptions").toggleClass('hide');
+});
+
+$("input[name='solidwaste']").on("click", function() {
+    $("#DrinkingwaterServiceOptions").toggleClass('hide');
+    $("#WastewaterServiceOptions").toggleClass('hide');
+    $("#SolidwasteServiceOptions").toggleClass('show');
 });
